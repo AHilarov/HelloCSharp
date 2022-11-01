53dc3f13a8155787e81f59cd4020b61f92be6c7f
AHilarov <ahilarov@yandex.ru>
2022-11-01 20:08:32 +0300

    условие если имя совподает

---
+++ Example 04. Condition (If-Else)/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What's your name?");
+string UserName = Console.ReadLine();
+if (UserName.ToLower == "alex")
+{
+Console.Write("Приветствую тебя, Alex!");
+}
+else
+{
+Console.Write("Hello, ");
+Console.Write(UserName);
+}
