36dabea50c5b4a4ad9f185a33549c692041cd33a
AHilarov <ahilarov@yandex.ru>
2022-11-01 23:17:22 +0300

    сам доработал прогу с именем

--- Example 04. Condition (If-Else)/Program.cs
+++ Example 04. Condition (If-Else)/Program.cs
@@ -1,6 +1,34 @@
-﻿Console.WriteLine("What's your name?");
+﻿// это правильный код, из лекции
+//Console.WriteLine("What's your name?");
+//string UserName = Console.ReadLine();
+//if (UserName.ToLower == "alex")
+//{
+//Console.Write("Приветствую тебя, Alex!");
+//}
+//else
+//{
+//Console.Write("Hello, ");
+//Console.Write(UserName);
+//}
+
+
+//Это мой апгрейт кода
+Console.WriteLine("What's your name?");
 string UserName = Console.ReadLine();
-if (UserName.ToLower == "alex")
+bool alex = false;
+
+//Console.WriteLine(alex);
+if (UserName == "Alex") alex = true;
+if (UserName == "ALEX") alex = true;
+if (UserName == "alex") alex = true;
+if (UserName == "Aleks") alex = true;
+if (UserName == "aleks") alex = true;
+if (UserName == "алекс") alex = true;
+if (UserName == "Алекс") alex = true;
+if (UserName == "АЛЕКС") alex = true;
+
+
+if (alex == true)
 {
 Console.Write("Приветствую тебя, Alex!");
 }
