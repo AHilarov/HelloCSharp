165a629aadd6380247ee8636dd80de0ec49dc23d
AHilarov <ahilarov@yandex.ru>
2022-11-01 03:29:55 +0300

    суммирование 2 случайных чисел от 0 до 9

---
+++ Example 03. Sum/Program.cs
@@ -0,0 +1,18 @@
+﻿// суммирование двух чисел
+//int Number1 = 100;
+//int Number2 = 13;
+//int Rusult = Number1 + Number2;
+//Console.WriteLine(Rusult);
+
+// суммирование 2 случайных чисел от 0 до 9
+Console.WriteLine("Суммирование 2 случайных чисел от 0 до 9");
+int NumberA = new Random().Next(1, 10); // 0 1 2 ... 9
+//Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1, 10); // 0 1 2 ... 9
+//Console.WriteLine(NumberB);
+int Rusult2 = NumberA + NumberB;
+Console.Write(NumberA);
+Console.Write(" + ");
+Console.Write(NumberB);
+Console.Write(" = ");
+Console.Write(Rusult2);
